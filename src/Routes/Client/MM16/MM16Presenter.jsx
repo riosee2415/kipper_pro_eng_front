1b4e3faebed14e6d7e0680e2f5103a4b6ca37f8d
박은비 <bag-eunbi@bag-eunbiui-MacBookPro.local>
2021-08-16 22:40:43 +0900

    modify

--- src/Routes/Client/MM16/MM16Presenter.jsx
+++ src/Routes/Client/MM16/MM16Presenter.jsx
@@ -486,7 +486,7 @@ const MM16Presenter = ({
             <Wrapper margin={`10px 0 0`} dr={`row`} ju={`space-between`}>
               <Wrapper width={`50%`} bgColor={Theme.white_C}>
                 <Image
-                  height={`465px`}
+                  height={`435px`}
                   isBlendMode={true}
                   src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/KEEPER_PRO%2Fassats%2Fimages%2FMM02%2Fgraph-image-4.png?alt=media&token=bd25000d-522f-428d-af3e-69b54c2e6726`}
                 />
@@ -496,7 +496,7 @@ const MM16Presenter = ({
                 width={`45%`}
                 al={`flex-start`}
                 ju={`space-between`}
-                height={`465px`}
+                height={`435px`}
               >
                 <Wrapper al={`flex-start`}>
                   <Image
@@ -608,7 +608,7 @@ const MM16Presenter = ({
                 <Image
                   height={`450px`}
                   isBlendMode={true}
-                  src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/KEEPER_PRO%2Fassats%2Fimages%2FMM02%2Fgraph-image-4.png?alt=media&token=bd25000d-522f-428d-af3e-69b54c2e6726`}
+                  src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/KEEPER_PRO%2Fassats%2Fimages%2FMM16%2F%E1%84%8B%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A5%E1%86%BC%E1%84%8F%E1%85%A9%E1%84%83%E1%85%B3-image-5.png?alt=media&token=a58af519-738d-4d57-83b2-b23c7f06b6c8`}
                 />
               </Wrapper>
             </Wrapper>
