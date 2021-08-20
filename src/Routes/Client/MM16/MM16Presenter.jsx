1d5987b05dbf5c1ad97c1845e38a62c90c404f3e
박은비 <bag-eunbi@bag-eunbiui-MacBookPro.local>
2021-08-21 01:51:04 +0900

    modify

--- src/Routes/Client/MM16/MM16Presenter.jsx
+++ src/Routes/Client/MM16/MM16Presenter.jsx
@@ -449,7 +449,6 @@ const MM16Presenter = ({
                 <ATag
                   width={`auto`}
                   href="https://www.bloomberg.com/news/articles/2011-06-27/human-errors-fuel-hacking-as-test-shows-nothing-prevents-idiocy"
-                  target="_blank"
                 >
                   <Text
                     color={Theme.subTheme2_C}
@@ -538,7 +537,6 @@ const MM16Presenter = ({
                 <ATag
                   width={`auto`}
                   href="https://arstechnica.com/information-technology/2013/01/two-us-power-plants-infected-with-malware-spread-via-usb-drive/"
-                  target="_blank"
                 >
                   <Wrapper
                     dr={`row`}
@@ -656,7 +654,6 @@ const MM16Presenter = ({
                 <ATag
                   width={`auto`}
                   href="https://thehackernews.com/2015/03/killer-usb-explode-computer.html"
-                  target="_blank"
                 >
                   <Wrapper al={`flex-start`} margin={`10px 0 0`}>
                     <Button dr={`row`} hoverBgColor={Theme.subTheme2_C}>
@@ -709,7 +706,6 @@ const MM16Presenter = ({
                 <ATag
                   width={`auto`}
                   href="https://www.theverge.com/2019/4/17/18412427/college-saint-rose-student-guilty-usb-killerdestroyed-computers"
-                  target="_blank"
                 >
                   <Wrapper al={`flex-start`} margin={`38px 0 0`}>
                     <Button dr={`row`} hoverBgColor={Theme.subTheme2_C}>
@@ -1236,7 +1232,6 @@ const MM16Presenter = ({
             <ATag
               width={`auto`}
               href="https://www.bloomberg.com/news/articles/2011-06-27/human-errors-fuel-hacking-as-test-shows-nothing-prevents-idiocy"
-              target="_blank"
             >
               <Text
                 margin={`25px 0 0`}
@@ -1319,7 +1314,6 @@ const MM16Presenter = ({
             <ATag
               width={`auto`}
               href="https://arstechnica.com/information-technology/2013/01/two-us-power-plants-infected-with-malware-spread-via-usb-drive/"
-              target="_blank"
             >
               <Wrapper
                 margin={`10px 0 0`}
@@ -1494,7 +1488,6 @@ const MM16Presenter = ({
             <ATag
               width={`auto`}
               href="https://www.theverge.com/2019/4/17/18412427/college-saint-rose-student-guilty-usb-killerdestroyed-computers"
-              target="_blank"
             >
               <Wrapper color={Theme.subTheme2_C} dr={`row`} width={`auto`}>
                 <Text fontSize={`14px`} fontWeight={`700`}>
