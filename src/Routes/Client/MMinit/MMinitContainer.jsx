60418bdca46e9de934ad4164c8bb5921b081ebdd
박은비 <bag-eunbi@bag-eunbiui-MacBookPro.local>
2021-08-21 01:51:31 +0900

    modify

--- src/Routes/Client/MMinit/MMinitContainer.jsx
+++ src/Routes/Client/MMinit/MMinitContainer.jsx
@@ -20,7 +20,7 @@ const MMinitContainer = ({ history }) => {
 
   ///////////// - EVENT HANDLER- ////////////
   const moveLinkHandler = (url) => {
-    window.open(url, "_blank");
+    window.open(url);
   };
 
   const moveLinkHandler2 = (link) => {
