bfbe6a82ab3bcae085d3e9dfeb1b81b25c4dde04
khj6407 <khj64076407@gmail.com>
2021-08-27 14:02:29 +0900

    homepage domain

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const uri =
   process.env.NODE_ENV === "production"
-    ? `https://www.schoolhealing4.co.kr/graphql`
+    ? `https://www.smartkeeper.kr/graphql`
     : "http://localhost:7068/graphql";
 
 export default new ApolloClient({
